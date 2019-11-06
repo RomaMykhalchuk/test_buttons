db437576171ea2709dc41197c0f5eef89767136a
Roman Mykhalchuk <romanmykhalchuk@gmail.com>
2019-11-06 10:57:18 +0200

    color theme and size changing were added

--- src/App.js
+++ src/App.js
@@ -7,33 +7,36 @@ import './App.scss';
 function App() {
 const [link,setLink] = useState('');
 const [currentTheme,setCurrentTheme] = useState('');
-
+const [currentSize, setCurrentSize] = useState('');
 
   return (
     <div className="App">
      
      <Button 
+      buttonSize={currentSize}
       isLink={link}
       colorTheme = {currentTheme}/>
      <input type="radio" id="radioButton1" name="a" value="link" onClick = {(e)=>{
        setLink(e.target.value);
      }}/>
-         <label htmlFor="radioButton1">Set the button as a link</label>
+    <label htmlFor="radioButton1">Set the button as a link</label>
      <input type="radio" id="radioButton2" name="a" value="not link" onClick = {(e)=>{
        setLink(e.target.value);
      }}/>
      <label htmlFor="radioButton2">Set the button as not a link</label> 
-     <select>
-        <option value="success" onClick={(e)=> {
-          setCurrentTheme(e.target.value)
-        }}>Success theme</option>
-        <option value="warning" onClick={(e)=> {
-          setCurrentTheme(e.target.value)
-        }} >Warning theme</option>
-        <option value="error" onClick={(e)=> {
-          setCurrentTheme(e.target.value)
-        }}>Error theme</option>
+     <select onChange={(e)=>{setCurrentTheme(e.target.value)
+     }}>
+        <option value="success">Success theme</option>
+        <option value="warning">Warning theme</option>
+        <option value="error">Error theme</option>
       </select>
+     <select onChange={(e)=>{
+       setCurrentSize(e.target.value);
+     }}>
+        <option value="small">Small</option>
+        <option value="medium">Medium</option>
+        <option value="large">Large</option>
+     </select>
     </div>
   );
 }
--- src/button/Button.js
+++ src/button/Button.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import './Button.scss';
 import './_size/button_size_s.scss';
+import './_size/button_size_m.scss';
+import './_size/button_size_l.scss';
 import './_link/button_link_default.scss';
-import './_color-theme/button_color-theme_success.scss'
+import './_color-theme/button_color-theme_success.scss';
+import './_color-theme/button_color-theme_error.scss';
+import './_color-theme/button_color-theme_warning.scss';
 
 const Button = (props) => {
-
-
-
+let currentThemeMode = '';
+let currentSize = '';
+switch(props.colorTheme) {
+  case 'success':
+   currentThemeMode = 'button_color-theme_success';
+   break;
+   case 'warning':
+      currentThemeMode = 'button_color-theme_warning';
+      break;
+      case 'error':
+          currentThemeMode = 'button_color-theme_error';
+          break;
+          default:
+              currentThemeMode = 'button';
+}
+switch(props.buttonSize) {
+  case 'small':
+   currentSize = 'button_size_s';
+   break;
+   case 'medium':
+      currentSize = 'button_size_m';
+      break;
+      case 'large':
+          currentSize = 'button_size_l';
+          break;
+          default:
+              currentSize = 'button';
+}
 
   return(
   <div>
     
-    <button className='button'>
+    <button className={`button ${currentThemeMode} ${currentSize}`}>
      
       {props.isLink === 'link' ? 
       <a href="https://football.ua/" className="button button_link_default">Click</a> : 'Click'}
